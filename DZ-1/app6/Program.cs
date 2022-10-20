f3968e45e18090fa063b8229cc965fc00f13592d
P E <bra811@mail.ru>
2022-10-20 13:11:58 +0300

    закинул часть 1 дз(app6+8)

---
+++ DZ-1/app6/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Привет, давай проверим четность!");
+Console.Write("Введи число - ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 0) Console.WriteLine($"Да, число {a} чётное!");
+else Console.WriteLine($"Нее, число {a} нечётное!");
---
+++ DZ-1/app8/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введи число - ");
+int n = Convert.ToInt32(Console.ReadLine());
+int a = 2;
+string reslt = ("");
+int count = 0;
+if (n > 4)
+{
+while (a < n)
+    {
+    reslt = reslt + a + ", ";
+    a = a + 2;
+    count++;
+    }
+}
+else 
+reslt = ($"{a}");
+/* 
+if (reslt[count*3] == ',')
+{
+    reslt[count*3] = ' ';
+}
+*/
+Console.Write(reslt);
