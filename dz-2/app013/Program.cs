1c46ea795500212992e52873507d2a83d7ecd8c5
P E <bra811@mail.ru>
2022-10-25 13:35:02 +0300

    fix dz2

--- dz-2/app013/Program.cs
+++ dz-2/app013/Program.cs
@@ -1,11 +1,12 @@
 ﻿Console.WriteLine("Выведу вам 3ю цифру в числе. Введите число");
 int ishodn = Convert.ToInt32(Console.ReadLine());
-if (ishodn < 100)
+int absol = Math.Abs(ishodn);
+if (absol < 100)
 {
    Console.WriteLine("Третьей цифры в этом числе нет!"); 
 }
 else
 {
-    string preobraz = Convert.ToString(ishodn);
+    string preobraz = Convert.ToString(absol);
     Console.WriteLine($"Третья цифра это - {preobraz[2]}");
 }
