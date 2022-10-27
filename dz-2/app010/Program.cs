925f58053fc458bc1405a3e66cf3d2f4d5a52b61
P E <bra811@mail.ru>
2022-10-27 19:34:31 +0300

    dz3 - done

--- dz-2/app010/Program.cs
+++ dz-2/app010/Program.cs
@@ -2,8 +2,8 @@
 int ishodn = Convert.ToInt32(Console.ReadLine());
 while (ishodn < 100 || ishodn > 999)
 {
-   Console.WriteLine("Не похоже на трёхзначное число.. Введите заново: "); 
- уп  ishodn = Convert.ToInt32(Console.ReadLine());
+  Console.WriteLine("Не похоже на трёхзначное число.. Введите заново: "); 
+  ishodn = Convert.ToInt32(Console.ReadLine());
 }
 int preobraz = (ishodn - ((ishodn/100)*100))/10;
 Console.WriteLine($"Вторая цифра это - {preobraz}");
