6c9e0470ecccde71d06cc37a95c353bd9250f0fa
P E <bra811@mail.ru>
2022-11-21 17:16:11 +0300

    dz6 sdelal

---
+++ dz-6/app041/Program.cs
@@ -0,0 +1,31 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//Console.Clear();
+Console.Write("Skolko chisel vvedete? ");
+int[] array = new int[Convert.ToInt32(Console.ReadLine())];
+Console.WriteLine();
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine($"Vvedite chislo {i + 1}: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+int CountPositiveNumbers(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) count++;
+    }
+    return count;
+}
+int coun = CountPositiveNumbers(array);
+Console.WriteLine($"Vi vveli '{coun}' polozhit chisel");
+
+Console.Write("Введите элементы (через пробел) :  ");
+int[] array2 = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+for (int i = 0; i < array2.Length; i++)
+{
+    Console.Write(array2[i]+", ");
+}
+    Console.WriteLine();
+    Console.WriteLine(array2[0]+array2[2]);
---
+++ dz-6/app043/Program.cs
@@ -0,0 +1,40 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+Console.WriteLine("Davai naydem tochku peresechenia 2 pramih tipa 'y = k*x + b'");
+double[] EnterParams()
+{
+    double[] a = new double[4];
+    Console.Write("Vvedi k1: ");
+    a[0] = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Vvedi b1: ");
+    a[1] = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Vvedi k2: ");
+    a[2] = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Vvedi b2: ");
+    a[3] = Convert.ToDouble(Console.ReadLine());
+    /*Console.WriteLine($"Okey, davay naydem tochku peresechenia pryamih 'y = {a[0]}*x + {a[1]}' i 'y = {a[2]}*x + {a[3]}'");
+    Console.Write("Schitayu");
+    for (int i = 0; i < 8; i++)
+    {
+        Thread.Sleep(300);
+        Console.Write(".");
+    }
+    Console.WriteLine();*/
+    return a;
+}
+double[] arr = EnterParams();
+while (arr[0] == arr[2])
+{
+    if (arr[1] == arr[3])
+    {
+        Console.WriteLine("Eto zhe odna i ta ze pramaya poluchilas! Vvedi raznie.");
+        arr = EnterParams();
+    }
+    else
+    {
+        Console.WriteLine("Eti dve pramie ze parallelni. Vvedi raznie 'K'.");
+        arr = EnterParams();
+    }
+}
+double x = (arr[3] - arr[1]) / (arr[0] - arr[1]);
+double y = arr[0] * x + arr[1];
+Console.WriteLine($"Tochka peresechenia poluchetsa takaya: x = {x} i y = {y}");
