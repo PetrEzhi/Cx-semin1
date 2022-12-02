90c57ce82864e97809a1a904049b668181ee09f8
P E <bra811@mail.ru>
2022-12-02 12:40:58 +0300

    дз-8

--- sem2/app069/Program.cs
+++ sem2/app069/Program.cs
@@ -5,8 +5,8 @@ Console.Write("Enter B: ");
 int B = Convert.ToInt32(Console.ReadLine());
 int Pow(int A, int B)
 {
-    //return B == 0 ? 1 : PowerRec(A, B - 1) * A;
+    //return B == 0 ? 1 : Pow(A, B - 1) * A;
     if (B == 0) return 1;
-    else return PowerRec(A, B - 1) * A;
+    else return Pow(A, B - 1) * A;
 }
 Console.WriteLine(Pow(A, B));
